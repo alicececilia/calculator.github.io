b94d909099a66b465648c950e1a60d224eabe92d
Alice Cecilia <alicececiliars900@gmail.com>
2025-08-16 19:17:34 -0300

    final project

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ class Calculator {
 
   // add digito na tela da calc
   addDigit(digit) {
-    //checando se a tela de operações já possui ponto .
+    //checando se a tela de operações já possui ponto "."
     if (digit === "." && this.currentOperationText.innerText.includes(".")) {
       return;
     }
@@ -21,20 +21,125 @@ class Calculator {
     this.updateScreen();
   }
 
-  // operadores
+  // processando todas as operações
   processOperation(operation) {
-    console.log(operation);
+    //check if valor atual está vazio
+    if (this.currentOperationText.innerText === "" && operation !== " C") {
+      // mudar operação
+      if (this.previousOperationText.innerText !== "") {
+        this.changeOperation(operation);
+      }
+      return;
+    }
+
+    // valore anteriores e atuais
+    let operationValue;
+    let previous = +this.previousOperationText.innerText.split(" ")[0];
+    let current = +this.currentOperationText.innerText;
+
+    switch (operation) {
+      case "+":
+        operationValue = previous + current;
+        this.updateScreen(operationValue, operation, current, previous);
+        break;
+
+      case "-":
+        operationValue = previous - current;
+        this.updateScreen(operationValue, operation, current, previous);
+        break;
+
+      case "/":
+        operationValue = previous / current;
+        this.updateScreen(operationValue, operation, current, previous);
+        break;
+
+      case "*":
+        operationValue = previous * current;
+        this.updateScreen(operationValue, operation, current, previous);
+        break;
+
+      case "DEL":
+        operationValue = previous * current;
+        this.processDelOperator();
+        break;
+
+      case "CE":
+        operationValue = previous * current;
+        this.processClearCurrentOperator();
+        break;
+
+      case "C":
+        operationValue = previous * current;
+        this.processClearAllOperator();
+        break;
+
+      case "=":
+        operationValue = previous * current;
+        this.processEqualOperator();
+        break;
+
+      default:
+        return;
+    }
   }
 
   // mudando valores na tela da calc
-  updateScreen() {
-    this.currentOperationText.innerText += this.currentOperation;
+  updateScreen(
+    operationValue = null,
+    operation = null,
+    current = null,
+    previous = null
+  ) {
+    if (operationValue === null) {
+      this.currentOperationText.innerText += this.currentOperation;
+    } else {
+      // checando se o valor é zero, se for apenas adicione o valor atual
+      if (previous === 0) {
+        operationValue = current;
+      }
+
+      // adicionando o valor atual ao anterior
+      // prettier-ignore
+      this.previousOperationText.innerText = `${operationValue} ${operation}`;
+      this.currentOperationText.innerText = "";
+    }
+  }
+
+  // mudando operação metemática
+  changeOperation(operation) {
+    const mathOperations = ["*", "/", "+", "-"];
+    if (!mathOperations.includes(operation)) {
+      return;
+    }
+
+    this.previousOperationText.innerText =
+      this.previousOperationText.innerText.slice(0, -1) + operation;
+  }
+
+  // deletando último número
+  processDelOperator() {
+    this.currentOperationText.innerText =
+      this.currentOperationText.innerText.slice(0, -1);
+  }
+  // limpando operação atual
+  processClearCurrentOperator() {
+    this.currentOperationText.innerText = "";
+  }
+  // limpando todas as operações
+  processClearAllOperator() {
+    this.currentOperationText.innerText = "";
+    this.previousOperationText.innerText = "";
+  }
+  // precessando a opp "="
+  processEqualOperator() {
+    let operation = previousOperationText.innerText.split(" ")[1];
+    this.processOperation(operation);
   }
 }
 
 const calc = new Calculator(previousOperationText, currentOperationText);
 
-// clique do botão para receber o valor do texto
+// clique do botão para receber o valor do texto (números e ".")
 buttons.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const value = e.target.innerText;
